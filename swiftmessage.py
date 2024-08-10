e41c1ad5113a57780bae75cefae4ed89a7f910f8
Swift <148490859+swiftmessage@users.noreply.github.com>
2024-08-10 16:42:27 +0300

    Update swiftmessage.py

--- swiftmessage.py
+++ swiftmessage.py
@@ -5,7 +5,7 @@ import webview
 url = "https://your_url"
 
 # create web app
-window = webview.create_window("Messenger", url, width=800, height=600)
+window = webview.create_window("Name app", url, width=800, height=600)
 
 # Запускаем окно
 webview.start()
